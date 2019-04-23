2bca79842e9b13657173f55fd1b7a544d8053ced
Wilfred Ruck <wilfredoruck@gmail.com>
2019-04-23 16:55:20 -0400

    replicate coding challenge

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { githubData: [] };
+  }
+
+  componentWillMount() {
+    fetch("https://api.github.com/users/wilfredruck/followers").then(
+      response => response.json()).then(
+        data => {
+          let info = []
+          for(let i = 0; i < data.length; i++) {
+            info.push({username: data[i].login, link: data[i].html_url, id: data[i].id});
+          }
+
+          this.setState({ githubData: info })
+        }
+      )
+  }
+  
+  render() {
+    const sortedInfo = this.state.githubData.sort((a,b) => a.id - b.id);
+    const usernames = sortedInfo.map((info, idx) => 
+      <li key={idx}> <a href={info.link}> {info.username} </a> </li>
+    )
+    const ids = sortedInfo.map((info, idx) =>
+      <li key={idx}> {info.id} </li>
+    )
+
+    return (
+      <div className="App">
+        <h1>Marketproof</h1>
+        <div className="data-table">
+          <ul>
+            {usernames}
+          </ul>
+          <ul>
+            {ids}
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
